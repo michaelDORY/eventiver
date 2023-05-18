41ed45c05be20caf885beb0b21b9ca9f109f74d0
michaelDORY <mish.makwww@gmail.com>
2023-05-19 02:27:35 +0300

    Fix, Update Readme

--- backend/README.md
+++ backend/README.md
@@ -6,7 +6,7 @@
 ---
 
 - documentation: https://documenter.getpostman.com/view/21408670/2s93kxb5aE
-- DEMO video: ...
+- DEMO video: https://drive.google.com/drive/folders/1B1iF302M-Lbinx2iTXG2SPM7fusV_zwa?usp=share_link
 
 ---
 
@@ -21,16 +21,19 @@
 ## HOW TO:
 
 ### Deploy:
+
 ```
 rsync -e "ssh -i ./eventiver.pem" -av --exclude={'dist','node_modules','eventiver.pem'} ./ ubuntu@16.171.45.211:/home/ubuntu/Code/eventiver
 ```
 
 ### Connect to Ubuntu:
+
 ```
 ssh ubuntu@16.171.45.211 -i ./eventiver.pem
 ```
 
 ### Run (on Ubuntu):
+
 ```
 cd Code/eventiver
 npm install
@@ -38,8 +41,15 @@ npm run build
 npm start
 ```
 
+### Run (local):
+
+```
+npm run dev
+```
+
 ---
 
 ###### REMEMBER:
+
 - eventiver.pem
 - .env
--- backend/src/middlewares/roles.js
+++ backend/src/middlewares/roles.js
@@ -4,43 +4,45 @@ const {
   ManagerModel,
   FoodProviderModel,
 } = require('../models')
-const { USER_ROLES } = require('../constants')
+const {USER_ROLES} = require('../constants')
 const ApiError = require('../utils/ApiError')
 
 const rolesMiddleware = (roles) => async (req, res, next) => {
-  const { userId } = req
+  const {userId} = req
 
   try {
     const realRole = (
       await UserModel.findOne({
-        where: { id: userId },
-        attributes: { include: ['role'] },
+        where: {id: userId},
+        attributes: {include: ['role']},
       })
     )?.dataValues.role
+
+    console.log('realRole', realRole)
 
     if (roles.includes(realRole)) {
       switch (realRole) {
         case USER_ROLES.guest:
           req.guestId = (
             await GuestModel.findOne({
-              where: { userId },
-              attributes: { include: ['id'] },
+              where: {userId},
+              attributes: {include: ['id']},
             })
           )?.dataValues.id
           break
         case USER_ROLES.manager:
           req.managerId = (
             await ManagerModel.findOne({
-              where: { userId },
-              attributes: { include: ['id'] },
+              where: {userId},
+              attributes: {include: ['id']},
             })
           )?.dataValues.id
           break
         case USER_ROLES.foodProvider:
           req.foodProviderId = (
             await FoodProviderModel.findOne({
-              where: { userId },
-              attributes: { include: ['id'] },
+              where: {userId},
+              attributes: {include: ['id']},
             })
           )?.dataValues.id
           break
